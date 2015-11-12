bbbf8ba9c3868e5cb2535e14d42cb32ab1639fa5
minaatefmaf <minaatefmaf@gmail.com>
2015-11-12 13:20:41 +0200

    Add a placeholder for the PhotoAlbumViewController

---
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAlbumViewController.swift
+//  Virtual Tourist
+//
+//  Created by Mina Atef on 11/12/15.
+//  Copyright © 2015 minaatefmaf. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+}
