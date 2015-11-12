620cb7599c33ed3532dbd3c45ef23bed861cfc91
minaatefmaf <minaatefmaf@gmail.com>
2015-11-12 13:33:34 +0200

    Create initial UI for the Photo Album View.

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -7,7 +7,13 @@
 //
 
 import UIKit
+import MapKit
 
 class PhotoAlbumViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var editingButton: UIButton!
+    
     
 }
