77c5a4e95ba9f8ec7ffc323f93418b99496bd794
minaatefmaf <minaatefmaf@gmail.com>
2015-11-14 00:04:19 +0200

    Annotate the map view in the photo album scene with the pin.

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import MapKit
 
-class PhotoAlbumViewController: UIViewController {
+class PhotoAlbumViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var collectionView: UICollectionView!
@@ -18,4 +18,35 @@ class PhotoAlbumViewController: UIViewController {
     // The pin to display (from TravelLocationsMapViewController)
     var thePin: Pin!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the delegate to this view controller
+        self.mapView.delegate = self
+        
+        // Annotate the map with the pin
+        annotateTheLocationOnMap()
+        
+        // Configure the UI
+        configureUI()
+    }
+    
+    func annotateTheLocationOnMap() {
+        // Get the location on the map
+        let coordinate = CLLocationCoordinate2D(latitude: thePin.latitude, longitude: thePin.longitude)
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegionMake(coordinate, span)
+        mapView.setRegion(region, animated: true)
+        
+        // Annotate the location
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = CLLocationCoordinate2D(latitude: thePin.latitude, longitude: thePin.longitude)
+        mapView.addAnnotation(annotation)
+    }
+        
+    func configureUI() {
+        // Prepare the map view
+        mapView.scrollEnabled = false
+        mapView.zoomEnabled = false
+    }
 }
