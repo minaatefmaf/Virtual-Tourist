129392e89921b847dcd0e94ba88f4b0ba785677d
minaatefmaf <minaatefmaf@gmail.com>
2015-11-14 19:30:13 +0200

    Add some constants to the FlikrConstants.swift

--- Virtual Tourist/FlikrConstants.swift
+++ Virtual Tourist/FlikrConstants.swift
@@ -10,4 +10,40 @@ import Foundation
 
 extension FlikrClient {
     
+    // MARK: - Constants
+    struct Constants {
+        
+        // MARK: Flikr API key
+        static let API_KEY: String = "ENTER_A_FLIKR_API_KEY_HERE"
+        
+        // MARK: URL
+        static let BASE_URL_SECURE: String = "https://api.flickr.com/services/rest/"
+        
+    }
+    
+    
+    // MARK: - Method Arguments
+    struct MethodsArgguments {
+        
+        static let METHOD_NAME = "flickr.photos.search"
+        static let EXTRAS = "url_m"
+        static let SAFE_SEARCH = "1"
+        static let DATA_FORMAT = "json"
+        static let NO_JSON_CALLBACK = "1"
+        
+    }
+    
+    
+    // MARK: - Bounding Box Stuff
+    struct BoundingBox {
+        
+        static let BOUNDING_BOX_HALF_WIDTH = 1.0
+        static let BOUNDING_BOX_HALF_HEIGHT = 1.0
+        static let LAT_MIN = -90.0
+        static let LAT_MAX = 90.0
+        static let LON_MIN = -180.0
+        static let LON_MAX = 180.0
+        
+    }
+    
 }
