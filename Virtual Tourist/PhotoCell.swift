1eb94c4f66597bc35109e38d1e5a86207bfe255b
minaatefmaf <minaatefmaf@gmail.com>
2015-11-15 13:08:08 +0200

    Add basic setup for the photo cell view.

---
+++ Virtual Tourist/PhotoCell.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCell.swift
+//  Virtual Tourist
+//
+//  Created by Mina Atef on 11/15/15.
+//  Copyright © 2015 minaatefmaf. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: TaskCancelingCollectionViewCell {
+    
+    @IBOutlet weak var frameImageView: UIImageView!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+}
---
+++ Virtual Tourist/TaskCancelingCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCancelingCollectionViewCell.swift
+//  Virtual Tourist
+//
+//  Created by Mina Atef on 11/15/15.
+//  Copyright © 2015 minaatefmaf. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class TaskCancelingCollectionViewCell: UICollectionViewCell {
+    
+    // The property uses a property observer. Any time its value is set it canceles the previous NSURLSessionTask.
+    var taskToCancelifCellIsReused: NSURLSessionTask? {
+        
+        didSet {
+            if let taskToCancel = oldValue {
+                taskToCancel.cancel()
+            }
+        }
+    }
+}
